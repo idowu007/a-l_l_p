ae4bcd76705508f24308beb5d05ef77d746068dd
idowu007 <adeyinkaidowu@gmail.com>
2023-02-19 17:04:52 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all the number of base16
+ * print from a - f
+ * with putchar for both and new line
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num <= 16; num++)
+		putchar((16 % 10) + '0');
+	for (la = 'a'; la <= f; la++)
+		putchar (la);
+	putchar('\n');
+s	return (0);
+}

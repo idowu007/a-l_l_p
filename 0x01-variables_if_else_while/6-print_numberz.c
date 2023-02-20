065d7aa14b26073dd0fa22398844e7de6fe2be56
idowu007 <adeyinkaidowu@gmail.com>
2023-02-19 16:26:56 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print single numbers of base 10 starting from 0, with new line
+ * with putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);

c958f5ccd54b0a91db3c213f5e3b89e02b0bca4d
idowu007 <adeyinkaidowu@gmail.com>
2023-02-19 16:04:51 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
